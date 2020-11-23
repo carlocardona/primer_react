52ceb2ef14649561372b457d8db3fe15a4372aae
Carlo C <pbjunkiep3@yahoo.com>
2020-11-23 21:23:07 +1100

    created func for even style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
-const count = 4;
-
-function isEven() {
-  return count % 2 === 0 ? "Even" : "Odd";
-}
-
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      count:4
+    }
+  }
+
+    isEven(){
+      return this.state.count % 2 === 0 ? "Even" : "Odd";
+    }
+
+    getClassName(val){
+      return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1"
+    }
+  
   render = () =>
-    <h4 className="bg-primary text-white text-center p-2 m-1"> Number of Things: {isEven()}</h4>
+    <h4 className={this.getClassName(this.state.count)}> Number of Things: {this.isEven(this.state.count)}</h4>
 }
