07a4468047d89e58ee52cc3534c2af8b92b78305
Carlo Cardona <carlo.dev2@gmail.com>
2020-11-23 13:58:18 +1100

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
+const count = 4;
+
+function isEven() {
+  return count % 2 === 0 ? "Even" : "Odd";
+}
+
 export default class App extends Component {
   render = () =>
-    <h4 className="bg-primary text-white text-center p-2 m-1">Component Element</h4>
+    <h4 className="bg-primary text-white text-center p-2 m-1"> Number of Things: {isEven()}</h4>
 }
